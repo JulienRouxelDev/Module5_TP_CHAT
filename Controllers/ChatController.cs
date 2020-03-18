ac90404868d61a3dfd17fb5bcdddca8e01122058
JulienRouxel <rouxel.julien@orange.fr>
2020-03-18 15:58:26 +0100

    Initial commit

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP.Models;
+
+namespace testAppliWeb.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        public ActionResult Index()
+        {
+            List<Chat> chats = Chat.GetMeuteDeChats();
+            return View(chats);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+
+            List<Chat> chats = Chat.GetMeuteDeChats();
+            Chat chatAAfficher = chats.Where(c => c.Id == id).First();
+            return View(chatAAfficher);
+        }
+
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            List<Chat> chats = Chat.GetMeuteDeChats();
+            Chat chatASuppr = chats.Where(c => c.Id == id).First();
+
+            return View(chatASuppr);
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
